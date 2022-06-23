11ede83a87cc2c0f41f02a9bc16759d423c5d4bb
Br-um <henriquedsdbrum@gmail.com>
2022-06-23 16:04:54 -0300

    Commit-6

--- public/js/init.js
+++ public/js/init.js
@@ -1,16 +1,18 @@
 console.log(window.innerWidth);
+console.log(window.scroll);
+const menuHamburguer = new MenuHamburguer("menuHamburguer", ".list", ".item");
+menuHamburguer.init();
 
-const menuHamburguer = new MenuHamburguer(
-    "menuHamburguer",
-    ".list",
-    ".item"
-);
-menuHamburguer.init()
-
-
+const navScroll = new NavScroll(
+    "nav"
+)
+navScroll.init();
 
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-    },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
+
+console.log("init on");
+console.log(window.screenY);
