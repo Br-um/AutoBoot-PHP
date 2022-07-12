2b2825cc061a6f26390fc3d77709f75bfcfc109a
Br-um <henriquedsdbrum@gmail.com>
2022-07-12 12:45:42 -0300

    Commit-Brum-14

--- views/registerpage.php
+++ views/registerpage.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/css/indentation.css">
     <link rel="shortcut icon" href="public/img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="../public/css/logreg.css">
     <title>AutoBoot-Register</title>
@@ -14,26 +13,48 @@
 <body>
     <div class="regBar">
         <section>
-            <form class="form" action="../controller/user-controller.php" method="POST">
-            <h3>REGISTRE-SE</h3>    
-            <fieldset>
-                    <input type="text" name="name" placeholder="Digite seu nome: ">
+            <!-- multistep form -->
+            <form id="msform" action="../controller/user-controller.php" method="POST">
+                <!-- progressbar -->
+                <ul id="progressbar">
+                    <li class="active">Account Setup</li>
+                    <li>Social Profiles</li>
+                    <li>Personal Details</li>
+                </ul>
+                <!-- fieldsets -->
+                <fieldset>
+                    <h2 class="fs-title">Create your account</h2>
+                    <h3 class="fs-subtitle">This is step 1</h3>
+                    <input type="text" name="email" placeholder="Email" />
+                    <input type="password" name="pass" placeholder="Password" />
+                    <input type="password" name="cpass" placeholder="Confirm Password" />
+                    <input type="button" name="next" class="next action-button" value="Next" />
                 </fieldset>
                 <fieldset>
-                    <input type="email" name="email" placeholder="Digite seu email: ">
+                    <h2 class="fs-title">Social Profiles</h2>
+                    <h3 class="fs-subtitle">Your presence on the social network</h3>
+                    <input type="text" name="twitter" placeholder="Twitter" />
+                    <input type="text" name="facebook" placeholder="Facebook" />
+                    <input type="text" name="gplus" placeholder="Google Plus" />
+                    <input type="button" name="previous" class="previous action-button" value="Previous" />
+                    <input type="button" name="next" class="next action-button" value="Next" />
                 </fieldset>
                 <fieldset>
-                    <input type="password" name="password" placeholder="Digite sua senha: ">
-                </fieldset>
-                <fieldset>
-                    <input type="tel" name="phone" placeholder="Digite seu número de celular: ">
-                </fieldset>
-                <fieldset>
-                <button type="submit">Cadastrar</button>
+                    <h2 class="fs-title">Personal Details</h2>
+                    <h3 class="fs-subtitle">We will never sell it</h3>
+                    <input type="text" name="fname" placeholder="First Name" />
+                    <input type="text" name="lname" placeholder="Last Name" />
+                    <input type="text" name="phone" placeholder="Phone" />
+                    <textarea name="address" placeholder="Address"></textarea>
+                    <input type="button" name="previous" class="previous action-button" value="Previous" />
+                    <input type="submit" name="submit" class="submit action-button" value="Submit" />
                 </fieldset>
             </form>
         </section>
     </div>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="../public/js/jquery.js"></script>
 </body>
 
 </html>
